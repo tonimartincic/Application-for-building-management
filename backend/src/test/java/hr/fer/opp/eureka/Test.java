78c8f1aa68d51709546771c9d396b67cc9e41b3c
Toni Martincic <toni.martincic@fer.hr>
2017-10-14 22:21:34 +0200

    dependencies added into project

---
+++ backend/src/test/java/hr/fer/opp/eureka/Test.java
@@ -0,0 +1,4 @@
+package hr.fer.opp.eureka;
+
+public class Test {
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,12 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import configureStore from './store/configureStore';
 import App from 'ui/components/App';
 
-const store = configureStore();
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <div>
-        <div>
-          <NavigationBar />
-        </div>
-        <Route path='/' component={App} />
-      </div>
-    </Router>
-  </Provider>,
+  <div>
+    <App/>
+  </div>
+  ,
 
   document.getElementById('root'),
 );
