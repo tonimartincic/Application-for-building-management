73a8c77cfbd3bb87f84ac13a4d29847977e4ac2b
Ivana Mrsic <ivana.mrsic@fer.hr>
2017-10-19 16:12:59 +0200

    user model added

---
+++ backend/src/main/java/hr/fer/opp/eureka/domain/User.java
@@ -0,0 +1,13 @@
+package hr.fer.opp.eureka.domain;
+
+public class User {
+
+  String id;
+
+  String role;
+
+  public User(String id, String role) {
+    this.id = id;
+    this.role = role;
+  }
+}
