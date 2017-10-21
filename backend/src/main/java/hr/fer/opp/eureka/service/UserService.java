e917b36ffae193fde3d682aa028b45b6c192200c
Toni Martincic <toni.martincic@fer.hr>
2017-10-21 17:08:34 +0200

    User refactored

--- backend/src/main/java/hr/fer/opp/eureka/service/UserService.java
+++ backend/src/main/java/hr/fer/opp/eureka/service/UserService.java
@@ -8,6 +8,6 @@ public interface UserService {
 
   List<User> getAll();
 
-  User getUserById(String id);
+  User getUserById(Long id);
 
 }
--- backend/src/main/java/hr/fer/opp/eureka/service/impl/UserServiceImpl.java
+++ backend/src/main/java/hr/fer/opp/eureka/service/impl/UserServiceImpl.java
@@ -32,7 +32,7 @@ public class UserServiceImpl implements UserService {
   }
 
   @Override
-  public User getUserById(String id) {
+  public User getUserById(Long id) {
     return userRepository.findById(id);
   }
 }
