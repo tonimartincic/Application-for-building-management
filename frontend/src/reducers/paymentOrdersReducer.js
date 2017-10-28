712a30274997ff23aae2617c4e20807f29888011
Toni Martincic <toni.martincic@fer.hr>
2017-10-28 15:04:54 +0200

    reducers added in root reducer

---
+++ frontend/src/reducers/paymentOrdersReducer.js
@@ -0,0 +1,7 @@
+import initialState from './initialState';
+
+export default function paymentOrdersReducer(state = initialState.announcements, action) {
+  switch (action.type) {
+
+  }
+}
---
+++ frontend/src/reducers/usersReducer.js
@@ -0,0 +1,7 @@
+import initialState from './initialState';
+
+export default function usersReducer(state = initialState.announcements, action) {
+  switch (action.type) {
+
+  }
+}
