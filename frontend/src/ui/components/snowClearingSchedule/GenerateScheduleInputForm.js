69ff857e0c6555d6a80589da2a15338a09d9876f
Ivana Mrsic <ivana.mrsic@fer.hr>
2017-12-27 18:28:22 +0100

    added multiple constraints when choosing dates on snow clearing schedule generator

--- frontend/src/ui/components/snowClearingSchedule/GenerateScheduleInputForm.js
+++ frontend/src/ui/components/snowClearingSchedule/GenerateScheduleInputForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Modal, Button, ControlLabel } from 'react-bootstrap';
+import { Modal, Button, ControlLabel, Alert } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { generateClicked } from '../../../actions/snowClearingSchedulesActions';
 import fetchSnowClearingSchedules from '../../../actionCreators/snowClearingSchedulesActionCreators';
 import { generateSnowClearingSchedule } from '../../../actionCreators/snowClearingSchedulesActionCreators';
 import * as constants from '../../../constants/values';
 import DatePicker from 'react-bootstrap-date-picker';
+import * as dateUtils from '../../../utils/DateUtil';
 
 class GenerateScheduleInputForm extends React.Component {
   constructor(props) {
@@ -14,6 +15,10 @@ class GenerateScheduleInputForm extends React.Component {
     this.state = {
       startDate: null,
       endDate: null,
+      alertVisible1: false,
+      alertVisible2: false,
+      startDateSelected: null,
+      endDateSelected: null,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,29 +29,83 @@ class GenerateScheduleInputForm extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    const startDate = `${this.state.startDate.substring(0, 4)}-${this.state.startDate.substring(5, 7)}-${this.state.startDate.substring(8, 10)}`;
-    const endDate = `${this.state.endDate.substring(0, 4)}-${this.state.endDate.substring(5, 7)}-${this.state.endDate.substring(8, 10)}`;
+    if (this.state.startDate === null || this.state.endDate === null) {
+      if (this.state.startDate === null) {
+        this.setState({
+          startDateSelected: true,
+        });
+      }
+      if (this.state.endDate === null) {
+        this.setState({
+          endDateSelected: true,
+        })
+      }
+    } else {
+      const currentDate = new Date();
+      const start = new Date(this.state.startDate.substring(0, 4), this.state.startDate.substring(5, 7) - 1, this.state.startDate.substring(8, 10));
+      const end = new Date(this.state.endDate.substring(0, 4), this.state.endDate.substring(5, 7) - 1, this.state.endDate.substring(8, 10));
 
-    this.props.generateSnowClearingSchedule(startDate, endDate);
-    this.props.generateClicked();
+      if (start > end) {
+        this.setState({
+          alertVisible1: true,
+        });
+      } else if (end < currentDate) {
+        this.setState({
+          alertVisible2: true,
+        });
+      } else {
+        let startDate;
+        if (start < currentDate) {
+          debugger;
+          startDate = dateUtils.constructDateString(currentDate.getDate(), currentDate.getMonth() + 1, currentDate.getFullYear());
+        } else {
+          startDate = `${this.state.startDate.substring(0, 4)}-${this.state.startDate.substring(5, 7)}-${this.state.startDate.substring(8, 10)}`;
+        }
+        const endDate = `${this.state.endDate.substring(0, 4)}-${this.state.endDate.substring(5, 7)}-${this.state.endDate.substring(8, 10)}`;
 
-    this.setState({
-      startDate: null,
-      endDate: null,
-    });
+        this.props.generateSnowClearingSchedule(startDate, endDate);
+        this.props.generateClicked();
+
+        this.setState({
+          startDate: null,
+          endDate: null,
+        });
+      }
+    }
   }
 
   onChangeStartDate = (value) => {
     this.setState({
-      startDate: value
+      startDate: value,
+      alertVisible1: false,
+      alertVisible2: false,
+      startDateSelected: false,
     });
   };
 
   onChangeEndDate = (value) => {
     this.setState({
-      endDate: value
+      endDate: value,
+      alertVisible1: false,
+      alertVisible2: false,
+      endDateSelected: false,
     });
   };
+
+  handleAlertDismiss1 = () => {
+    this.setState({ alertVisible1: false });
+  }
+
+  handleAlertDismiss2 = () => {
+    this.setState({ alertVisible2: false });
+  }
+
+  handleAlertDismiss3 = () => {
+    this.setState({
+      startDateSelected: false,
+      endDateSelected: false,
+    });
+  }
 
   render() {
     return (
@@ -78,6 +137,31 @@ class GenerateScheduleInputForm extends React.Component {
               monthLabels={constants.monthNames}
               onChange={this.onChangeEndDate}
             />
+            <br/>
+            <Choose>
+              <When condition={this.state.alertVisible1}>
+                <Alert bsStyle="danger" onDismiss={() => this.handleAlertDismiss1()}>
+                  <h4>Pogrešno uneseni željeni datumi generiranja rasporeda</h4>
+                  <p>Početni datum generiranja rasporeda treba biti prije od završnog datuma generiranja rasporeda.</p>
+                </Alert>
+              </When>
+            </Choose>
+            <Choose>
+              <When condition={this.state.alertVisible2}>
+                <Alert bsStyle="danger" onDismiss={() => this.handleAlertDismiss2()}>
+                  <h4>Pogrešno uneseni željeni datumi generiranja rasporeda</h4>
+                  <p>Željeni datumi generiranja rasporeda su već prošli. Odaberite datum u budućnosti.</p>
+                </Alert>
+              </When>
+            </Choose>
+            <Choose>
+              <When condition={this.state.startDateSelected || this.state.endDateSelected}>
+                <Alert bsStyle="danger" onDismiss={() => this.handleAlertDismiss3()}>
+                  <h4>Pogrešno uneseni željeni datumi generiranja rasporeda</h4>
+                  <p>Oba datuma moraju biti odabrana kako bi se mogao generirati raspored čišćenja snijega.</p>
+                </Alert>
+              </When>
+            </Choose>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.handleSubmit}>Potvrdi</Button>
--- frontend/src/utils/DateUtil.js
+++ frontend/src/utils/DateUtil.js
@@ -1,7 +1,7 @@
 import * as constants from '../constants/values';
 
 export const constructDateString = (day, month, year) =>
-  day + ". " + constants.monthNamesGen[month - 1] + " " + year + ".";
+  year + '-' + month + '-' + day;
 
 export const constructDateStringForBackend = (date) => {
   const monthTemp = date.clearingDate.split('-')[1];
