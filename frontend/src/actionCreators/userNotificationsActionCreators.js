a215cae2167cfc03ed832446dad80885ea64167a
Marko Cavalli <marko.cavalli@fer.hr>
2018-01-05 00:28:14 +0100

    displaying notifications for user with bug

--- frontend/src/actionCreators/userNotificationsActionCreators.js
+++ frontend/src/actionCreators/userNotificationsActionCreators.js
@@ -1,9 +1,24 @@
 import axios from 'axios';
 import * as types from '../actions/actionTypes';
 
-export default async function fetchUserNotifications() {
+export async function fetchUserNotifications() {
   try {
     const response = await axios.get('/api/user-notifications');
+
+    return {
+      type: types.FETCH_USER_NOTIFICATIONS_SUCCESS,
+      data: response.data,
+    };
+  } catch (err) {
+    return {
+      type: types.FETCH_USER_NOTIFICATIONS_FAILURE,
+      data: err,
+    };
+  }
+}
+export default async function fetchUserNotificationsForUser(id) {
+  try {
+    const response = await axios.get('/api/user-notifications/'+id);
 
     return {
       type: types.FETCH_USER_NOTIFICATIONS_SUCCESS,
--- frontend/src/ui/components/notifications/Notifications.js
+++ frontend/src/ui/components/notifications/Notifications.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { NavDropdown, MenuItem } from 'react-bootstrap';
-//import {toggleUserNotification} from "../../../actions/userNotificationsActions";
 import {withRouter} from 'react-router-dom';
 import styles from './notifications.css';
 import fetchUserNotifications from "../../../actionCreators/userNotificationsActionCreators";
+import fetchUserNotificationsForUser from "../../../actionCreators/userNotificationsActionCreators";
+
 
 class Notifications extends Component {
-  componentDidMount() {
-    this.props.fetchUserNotifications();
+   id=this.props.userData.id;
+
+   componentDidMount() {
+    this.props.fetchUserNotificationsForUser(this.props.userData.id);
   }
+
   constructor(props) {
     super(props);
   }
@@ -40,12 +44,14 @@ class Notifications extends Component {
 function mapStateToProps(state) {
   return {
     userNotifications: state.userNotifications,
+    userData: state.userData,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     fetchUserNotifications: () => dispatch(fetchUserNotifications()),
+    fetchUserNotificationsForUser: (id) => dispatch(fetchUserNotificationsForUser(id)),
   };
 }
 
