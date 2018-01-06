ed3612ceef65c0e363fa0f3435deb7a511f5fe5a
Marko Cavalli <marko.cavalli@fer.hr>
2018-01-06 12:07:47 +0100

    finished user notificatons view

--- frontend/src/actionCreators/userNotificationsActionCreators.js
+++ frontend/src/actionCreators/userNotificationsActionCreators.js
@@ -21,7 +21,6 @@ export async function readNotificationsForUser() {
   try {
     const userId = JSON.parse(localStorage.getItem('user')).id;
     const response = await axios.put('/api/user-notifications-read/'+userId);
-    console.log(response);
     return {
       type: types.READ_USER_NOTIFICATIONS_SUCCESS,
       data: response.data,
